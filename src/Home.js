589154c1d15e52f1dfcaeccfa3dac3947d40c8fa
Hawaa <hawaazaroual@gmail.com>
2021-09-29 13:42:11 -0600

    start adding styling

--- src/Home.js
+++ src/Home.js
@@ -1,95 +1,83 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import './Home.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./Home.css";
 import useApplicationData from "./hooks/useApplicationData";
-import Nav from './components/Nav';
-import Account from './components/Account';
-import MyBudgets from './components/MyBudgets';
-import Sidebar from './components/Sidebar';
-import Budget from './components/budgets/index';
-import Pie from './components/charts/Pie';
-import BarGraph from './components/charts/BarGraph';
-import CreateBudget from './components/budgets/Budget_Create';
-import LeftOver from './components/charts/LeftOver';
-
- 
-
+import Nav from "./components/Nav";
+import Account from "./components/Account";
+import MyBudgets from "./components/MyBudgets";
+import Sidebar from "./components/Sidebar";
+import Budget from "./components/budgets/index";
+import Pie from "./components/charts/Pie";
+import BarGraph from "./components/charts/BarGraph";
+import CreateBudget from "./components/budgets/Budget_Create";
+import LeftOver from "./components/charts/LeftOver";
 
 const Home = () => {
-  const {
-    state,
-    addCategories
-  } = useApplicationData();
+  const { state, addCategories } = useApplicationData();
   return (
     <div className="App">
-
-    <Router>
-      <nav>
-<Nav name={state.username}/>
-        <div>
-        <Sidebar email={state.email}/>
-        </div>
-
-      </nav>
-
-      <Switch>
-
-        <Route path="/Account">
+      <Router>
+        <nav>
+          <Nav name={state.username} />
+          </nav>
+          <div>
+            <Sidebar email={state.email} />
+          </div>
         
-          </Route>
+
+        <Switch>
+          <Route path="/Account"></Route>
 
           <Route path="/Create">
-            <CreateBudget/>
+            <CreateBudget />
           </Route>
 
           <Route path="/MyBudgets">
             <MyBudgets />
-
           </Route>
 
           <Route path="/">
-            <Account />
-<Pie categories= {state.categories}
-           expenses={state.expenses}/>
-
-           <BarGraph categories= {state.categories}
-           expenses={state.expenses}/>
-
-           <LeftOver />
-
+            <body id="homepage">
+              <Account />
+              <div>
+                <Pie categories={state.categories} expenses={state.expenses} />
+                <br />
+                <BarGraph
+                  categories={state.categories}
+                  expenses={state.expenses}
+                />
+                <br />
+                <LeftOver />
+              </div>
+            </body>
           </Route>
-        
         </Switch>
-        
-        </Router>
-        </div>
-        
-        )
-      };
+      </Router>
+    </div>
+  );
+};
 
-      export default Home;
-      
-      
-      
-      // export default function Home() {
-      //   const {
-      //        state,
-      //        addCategories
-      //   } = useApplicationData();
-        
-      //  console.log('state', state);
-      //   return (
-      //     <div className="App">
-      //      <Nav name={state.username}/>
-      //      <Sidebar email={state.email}/>
-      //      <Account />
-      //      <Update />
-           
-      //      <Budget/>
-      //       <Pie categories= {state.categories}
-      //      expenses={state.expenses}/>
-      //      <BarGraph categories= {state.categories}
-      //      expenses={state.expenses}/>
-      //     </div>
-      //   );
-      // };
+export default Home;
+
+// export default function Home() {
+//   const {
+//        state,
+//        addCategories
+//   } = useApplicationData();
+
+//  console.log('state', state);
+//   return (
+//     <div className="App">
+//      <Nav name={state.username}/>
+//      <Sidebar email={state.email}/>
+//      <Account />
+//      <Update />
+
+//      <Budget/>
+//       <Pie categories= {state.categories}
+//      expenses={state.expenses}/>
+//      <BarGraph categories= {state.categories}
+//      expenses={state.expenses}/>
+//     </div>
+//   );
+// };

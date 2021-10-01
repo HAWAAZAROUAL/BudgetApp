b355613b5ef365cd9feaebf89157efb162dd56a7
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-10-01 00:47:09 +0000

    Create budgets page styles

--- src/components/budgets/index.js
+++ src/components/budgets/index.js
@@ -44,24 +44,38 @@ export default function CreateBudget() {
   return (
     <>
     <div className="new-budget">
+      <div className="title-boxes">Create new budget</div>
+      <div>
      <Form
              onSave={createBudget} 
              userId={state.userId} 
              
              />
              </div>
-    <div className="App">
-      <table border="1" frame="void" rules="rows" className="budget-table">
+             </div>
+    <div className="app">
+      <p className="title-boxes">Your Budgets</p>
+      
+      {/* <table border="1" frame="void" rules="rows" className="budget-table">
         <thead>
           <tr>
-            <th>BudgetName</th>
-            <th>BudgetLimit</th>
+            <th>Name</th>
+            <th>Limit</th>
             <th>StartDate</th>
             <th>EndDate</th>
           </tr>
         </thead>
         {budgets}
-      </table>
+      </table> */}
+
+      <div>
+        <span className="budget-table title">Title</span>
+        <span className="budget-table title">Limit</span>
+        <span className="budget-table title">Start</span>
+        <span className="budget-table title">End</span>
+        {budgets}
+      </div>
+      
     </div>
     </>
   );

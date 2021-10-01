54c8dbd7e1bdc87d2821f4fac8d99b0e7ae738dc
Hawaa <hawaazaroual@gmail.com>
2021-10-01 07:07:04 -0600

    begin display budgets created on myBudgets page

--- src/components/MyBudgets.js
+++ src/components/MyBudgets.js
@@ -1,3 +1,8 @@
+// TODO: REPLACE BUDGET NAME WITH THE CREATED BUDGET NAMES
+// ADD ALL THE CREATED BUDGETS TO THE MY BUDGETS PAGE
+
+
+
 import React from 'react';
 import Button from './Button';
 import './Update.css';
@@ -5,11 +10,20 @@ import "./Button.css";
 // import budgets from '../../budget_api/src/routes/budgets';
 
 export default function MyBudgets () {
-
+  // const displayData = function(categories, expenses) {
+  //   if () {
+  //     return Object.keys(expenses).map((expenseId) => {
+  //       return {
+  //         key: categories[expenses[expenseId].category_id].category_type,
+  //         data: expenses[expenseId].amount
+  //       }
+  //     })
+  //   }
+  // }
   return(
     <>
     <form>
-   {/* <h3> {this.budgets.name} </h3> */}
+   <h3> Budget Name </h3>
     <p>Add Category</p> <input type='text'></input> <Button id = "add-category">Add</Button>
     <br>
 

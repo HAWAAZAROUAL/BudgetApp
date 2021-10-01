f7206350240aaa43244ff7316c0f6645b39992e6
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-10-01 17:01:19 +0000

    No changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,30 +24,27 @@ export default class App extends React.Component {
   }
   render() {
     return (
-
       // Sidebar Element Declaration
       <div className="control-section">
         <SidebarComponent
-        
           id="default-sidebar"
           ref={(Sidebar) => (this.sidebarobj = Sidebar)}
           style={{ visibility: 'hidden' }}
           width={this.width}
           created={this.onCreate}
         >
-          <div
-            className="sidebar-header"
-          >
-            
-          </div>
+          <div className="sidebar-header"></div>
           <ul className="nav sidebar-nav">
-          
-            <div> <b>Alice Smith</b></div>
+            <div>
+              {' '}
+              <b>Alice Smith</b>
+            </div>
             <li className="list-items">
               <Link path to="/">
-              <style>
-@import url('https://fonts.googleapis.com/css2?family=Dosis:wght@500&display=swap');
-</style>
+                <style>
+                  @import
+                  url('https://fonts.googleapis.com/css2?family=Dosis:wght@500&display=swap');
+                </style>
                 <span className="e-text"> Home</span>
               </Link>
             </li>

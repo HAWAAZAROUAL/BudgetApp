be547e2658a4037e0ea1e341dedc750fe61cb730
amytang1234 <huilitang1@gmail.com>
2021-10-03 23:28:25 -0700

    add an SAVE alert

--- src/components/showMyBudgets.js
+++ src/components/showMyBudgets.js
@@ -1,10 +1,15 @@
 export default function ShowMybudgets(props) {
+  let alert;
+  if(Number(props.available)<=0){
+    alert="SAVE";
+  }
   return (
     <>
       <div>
         <span className="budget-table">{props.name}</span>
         <span className="budget-table">{props.budget}</span>
         <span className="budget-table">{props.available}</span>
+        <span className="budget-table">{alert}</span>
         <hr></hr>
       </div>
     </>

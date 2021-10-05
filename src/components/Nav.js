deef195018b486870803bab2ad78246c50a2a873
Hawaa <hawaazaroual@gmail.com>
2021-10-04 19:47:44 -0600

    change navbar, add logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import './Nav.css'
+import BudgetBuddyLogo from '../BudgetBuddyLogo.png';
 // import {Menu} from '../Menu.gif';
 
 // font style
@@ -11,8 +12,10 @@ export default function Nav(props) {
         url('https://fonts.googleapis.com/css2?family=Arima+Madurai&display=swap');
       </style>
       {/* <h1>{props.name}</h1> */}
-      <h1>Budget Buddy</h1>
-      <h5><em>Be your money's master, not its slave.</em></h5>
+      <div>
+        <img className="logo" src={BudgetBuddyLogo} alt="Logo"/>
+      </div>
+      <h4><em>Be your money's master, not its slave.</em></h4>
     </div>
   )
 }
